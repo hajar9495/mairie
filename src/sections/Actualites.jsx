c17b9aecec9b416f437c9c0e528bc3c293d26120
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-05-12 11:28:01 +0200

    actualites et menu arrangé

--- src/sections/Actualites.jsx
+++ src/sections/Actualites.jsx
@@ -7,26 +7,28 @@ const Actualites = ({ className }) => {
   return (
     <>
       <div className={className}>
-        <h1>Actualites</h1>
-        <div className=" grid grid-cols-1 gap-y-5 mx-auto ">
+        <h1 className="mx-3">Actualites</h1>
+        <div className=" grid grid-cols-1 gap-y-5 mx-3 ">
           <div className="bg-grisActualite w-full md:w-400 lg:w-72 h-32 mx-auto grid grid-cols-3">
             <div className="col-span-1">
               <img src={actu1} className="h-auto" alt="" />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 text-sm px-2">
               <h2>titre actualite 1</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Voluptatem tempore voluptates .
               </p>
+              <a href="" className="underline cursor-auto">
+                voir plus
+              </a>
             </div>
           </div>
           <div className="bg-grisActualite w-full md:w-400 lg:w-72 h-32 mx-auto grid grid-cols-3">
             <div className="col-span-1">
-              {" "}
               <img src={actu2} className="h-auto" alt="" />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 text-sm px-2">
               <h2>titre actualite 1</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -38,7 +40,7 @@ const Actualites = ({ className }) => {
             <div className="col-span-1">
               <img src={actu3} className="h-auto" alt="" />
             </div>
-            <div className="col-span-2">
+            <div className="col-span-2 text-sm px-2">
               <h2>titre actualite 1</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.

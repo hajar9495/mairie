4932aa0abe3e558c12a8339f3d9e9a11724ae198
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-05-15 00:12:21 +0200

    version 1 bug avec les tabs en dark mode

--- src/sections/TabsEspace.jsx
+++ src/sections/TabsEspace.jsx
@@ -8,7 +8,7 @@ const TabsEspace = () => {
     <>
       <Tabs aria-label="Default tabs" style="default">
         <Tabs.Item active title="Espace Perso">
-          <EspacePerso className={"w-11/12 flex justify-center"}></EspacePerso>
+          <EspacePerso className={"w-11/12 flex justify-center "}></EspacePerso>
         </Tabs.Item>
         <Tabs.Item title="creer un compte">
           <SeConnecter className={"w-11/12 flex justify-center"}></SeConnecter>
